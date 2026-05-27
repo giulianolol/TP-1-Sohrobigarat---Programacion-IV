1b1ebbee7bc9381b0f1dc2b59be70366e80d4ce2
Giuliano Sohrobigarat <gino.sohro@yahoo.com.ar>
2026-05-27 09:18:59 -0300

    Refactor encuestas to use signal for state management

--- src/app/pages/resultados-encuesta/resultados-encuesta.ts
+++ src/app/pages/resultados-encuesta/resultados-encuesta.ts
@@ -1,10 +1,11 @@
 import {
   Component,
   OnInit,
-  ChangeDetectorRef
+  signal
 } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+
 import { supabase } from '../../services/auth';
 
 @Component({
@@ -14,24 +15,23 @@ import { supabase } from '../../services/auth';
   templateUrl: './resultados-encuesta.html',
   styleUrls: ['./resultados-encuesta.css']
 })
+
 export class ResultadosEncuesta implements OnInit {
 
-  encuestas: any[] = [];
-
-  constructor(
-    private cdr: ChangeDetectorRef
-  ) {}
+  encuestas = signal<any[]>([]);
 
   async ngOnInit() {
 
     const { data } = await supabase
+
       .from('encuestas')
+
       .select('*')
-      .order('created_at', { ascending: false });
 
-    this.encuestas = data || [];
+      .order('created_at', {
+        ascending: false
+      });
 
-    this.cdr.detectChanges();
+    this.encuestas.set(data || []);
   }
-
 }
